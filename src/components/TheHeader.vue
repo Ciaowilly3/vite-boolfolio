715c906671a65faf48f3cdb1f963501f1e31f249
Ciaowilly3 <muscomassi@gmail.com>
2023-02-16 15:56:53 +0100

    dati ricevuti

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid">
+    <nav
+      class="navbar navbar-expand-lg navbar-light bg-light justify-content-center"
+    >
+      <a class="navbar-brand" href="#">Navbar</a>
+      <div id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Home <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
---
+++ src/pages/projects/index.vue
@@ -0,0 +1,28 @@
+<script>
+import { onMounted } from "vue";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      backendUrl: "http://127.0.0.1:8000",
+      projects: [],
+    };
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(this.backendUrl + "/api/projects/").then((resp) => {
+        this.projects = resp.data;
+      });
+    },
+  },
+  mounted() {
+    this.fetchProjects();
+  },
+};
+</script>
+
+<template>
+  <div v-for="project in projects" :key="project.id">
+    {{ project.name }}
+  </div>
+</template>
