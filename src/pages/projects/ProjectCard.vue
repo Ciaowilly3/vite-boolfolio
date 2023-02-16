61d2a1a9513674bb72ac820d09c5a2051566baf3
Ciaowilly3 <muscomassi@gmail.com>
2023-02-16 16:23:03 +0100

    card stampate ma immagine non reperibile

---
+++ src/pages/projects/ProjectCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: {
+    project: Object,
+  },
+  data() {
+    return {
+      backendUrl: "http://127.0.0.1:8000",
+    };
+  },
+};
+</script>
+<template>
+  <div class="card" style="width: 18rem">
+    <img
+      class="card-img-top"
+      :src="backendUrl + '/storage/' + project.cover_img"
+      alt="Card image cap"
+    />
+    <div class="card-body">
+      <h5 class="card-title">{{ project.name }}</h5>
+      <p class="card-text">
+        {{ project.description }}
+      </p>
+    </div>
+  </div>
+</template>
